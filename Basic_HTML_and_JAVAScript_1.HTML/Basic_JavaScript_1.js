2fc1be7376678656710a7de4ee8b193d256bca4c
Jason <jdhill@global.t-bird.edu>
2019-07-22 10:21:41 -0700

    Most recent CSS assignment

---
+++ Basic_HTML_and_JAVAScript_1.HTML/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Mmmm pretzel bites.");
+
+function My_First_Function() {
+var String = "Kiss me! I'm a mutt.";
+var result = String.fontcolor("green");
+document.getElementById("Mutt").innerHTML = result;
+}
