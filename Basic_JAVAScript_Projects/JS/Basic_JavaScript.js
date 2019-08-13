1ddc7a6b52b56d95465e4329e10c96a6e48134ac
Jason <jdhill@global.t-bird.edu>
2019-08-13 15:51:37 -0700

    Re repository

---
+++ Basic_JAVAScript_Projects/JS/Basic_JavaScript.js
@@ -0,0 +1,34 @@
+
+
+{
+    var str = "Hello World!<br><br>";
+    str = str.fontcolor("green");
+    document.write(str)
+}
+
+
+
+
+{
+    var Family= "The Hills", Dad = "David", Mom = "Diane", Daughter = "Anna", Son = "Jason";
+    
+    Dad = Dad.fontcolor("red");
+    Mom = Mom.fontcolor("yellow");
+    Daughter = Daughter.fontcolor("blue")
+    Son = Son.fontcolor("purple")
+    document.write(Son);
+    
+}
+
+document.write("<br> \"Keep cool but don\'t freeze.\"<br>-Mayonaise jar<br><br>");
+{
+   document.write(3 + 6);
+   document.write("<br>");
+}
+
+
+
+function My_First_Function() {
+    var String = "This Text is Green.";
+    var result = String.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;}
