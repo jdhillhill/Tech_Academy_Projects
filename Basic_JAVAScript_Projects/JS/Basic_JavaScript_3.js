b98951e786d632b5a61f8e72305c7f3c3b0e0109
Jason <jdhill@global.t-bird.edu>
2019-08-14 14:22:53 -0700

    JS Project #3
    Math functions

---
+++ Basic_JAVAScript_Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,39 @@
+
+
+
+   
+function frog(num1, num2) {
+    return num1 + num2;
+}
+
+function worm(num1, num2) {
+    return num1 - num2;
+}
+
+function rabbit(num1, num2) {
+    return num1 * num2;
+}
+
+function shark(num1, num2) {
+    return num1/ num2;
+}
+
+function platipus(num1, num2) {
+    return num1/ num2 + num1;
+}
+
+function remainder(num1, num2) {
+    return num1 % num2;
+}
+
+function negation(num1) {
+    return -num1;
+}
+
+function increment(num1) {
+    return num1++;
+}
+
+function decrement(num1) {
+    return num1--;
+}
