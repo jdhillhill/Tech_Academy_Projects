ae79f29151e41bb7f685bb4d88bf89f3971746de
Jason <jdhill@global.t-bird.edu>
2019-08-15 10:54:59 -0700

    JS Project #5

---
+++ Basic_JAVAScript_Projects/JS/Basic_JavaScript_5.js
@@ -0,0 +1,23 @@
+
+            function my_Function() {
+                document.getElementById("Test").innerHTML = 0/0;
+            }
+            function my_Function2() {
+                document.getElementById("Test2").innerHTML = isNaN("This is a string");
+            }
+
+            function my_Function3() {
+                document.getElementById("Test3").innerHTML = isNaN('007');
+            }
+
+            function myFunction() {
+                var x = 7;
+                document.getElementById("demo").innerHTML = (x === "7");
+            }
+            function myFunction2() {
+                var x = 7;
+                document.getElementById("demo2").innerHTML = (7 === 7);
+            }
+            function not_Function() {
+                document.getElementById("Not").innerHTML = !(5 > 10);
+            }
